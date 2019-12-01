0cd511be2373901e491a9e30db00c1b1fce134e7
dpalouktsis <t8170095@aueb.gr>
2019-12-01 14:08:39 +0200

    'maincases'

---
+++ MainERP.java
@@ -0,0 +1,62 @@
+package TeamProject;
+
+import java.util.Scanner;
+
+public class MainERP {
+	public static void ExInserts() { // we are inserting the same id for everybody cause it automatically changes//
+		Employee E1 = new Employee("George", "Papadopoulos", "01/02/1980", "Oxford Street 4", 0, "76372", "single");
+		Employee E2 = new Employee("John", "Lennon", "11/04/1960", "Weird Street 90", 0, "52543", "married");
+		Employee E3 = new Employee("Alex", "Turner", "1/01/1988", "Weirded Street 14", 0, "34242", "single");
+		Employee E4 = new Employee("James", "Hetfield", "21/02/1972", "Scary Street 47", 0, "09772", "married");
+		Employee E5 = new Employee("Sam", "Carter", "30/12/1990", "Spooky Street 5", 0, "798782", "single");
+
+	}
+
+	public static void main(String[] args) {
+		ExInserts();
+
+		int choice = -1;
+		boolean b = false;
+		while (!b) {
+			if (choice == -1) {
+				System.out.println("Welcome to our menu!");
+				System.out.println("Here are your choices:");
+				System.out.println("1.Add an employee");
+				System.out.println("2.Erase an employee");
+				System.out.println("3.Search and edit an employee");
+				System.out.println("4.Log out/Exit system");
+				Scanner sc1 = new Scanner(System.in);
+
+				choice = sc1.nextInt();
+			}
+			switch (choice) {
+			case 1:
+				Scanner sc2 = new Scanner(System.in);
+				String name = sc2.nextLine();
+				String surname = sc2.nextLine();
+				String dob = sc2.nextLine();
+				String adress = sc2.nextLine();
+				int id = sc2.nextInt();
+				String phonenum = sc2.nextLine();
+				String status = sc2.nextLine();
+				Employee e1 = new Employee(name, surname, dob, adress, id, phonenum, status); // The id of the employee
+																								// is irrelevant//
+				System.out.println("The Employee has been added to the system");
+				choice = -1;
+				continue;
+			case 2:
+				System.out.println("By which criteria do you want to search?");
+				Scanner sc3 = new Scanner(System.in);
+				String crit = sc3.nextLine();
+				Employee_Deletion.DeletionSearch(crit);
+				System.out.println("Select the id of the Employee you want to erase");
+				int id1 = sc3.nextInt();
+				Employee_Deletion.DeletionFin(id1);
+				System.out.println("The Employee has been erased from the system");
+				choice = -1;
+			case 3:
+				
+			}
+		}
+	}
+}
