b1ea48b2547a4eef82d5630735d5e248bee02469
cnteco <47892188+cnteco@users.noreply.github.com>
2019-12-11 19:52:43 +0200

    Add files via upload

--- Evaluation.java
+++ Evaluation.java
@@ -1,22 +1,16 @@
-package TeamProject;
-
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class Evaluation {
 
 	protected int empID;
 	protected String date;
-	protected int skillA;
-	protected int skillB;
-	protected int skillC;
+	public HashMap<String, Integer> skills = new HashMap<String, Integer>();
 
-	public Evaluation(int i, String d, int a, int b, int c) {
+	public Evaluation(int i, String d) {
 		this.empID = i;
 		this.date = d.substring(0, 2) + "/" + d.substring(2, 4) + "/" + d.substring(4, 8);
-		this.skillA = a;
-		this.skillB = b;
-		this.skillC = c;
 	}
 
 	public int getEmpID() {
@@ -30,7 +24,7 @@ public class Evaluation {
 	public String getDate() {
 		return date;
 	}
-
+	
 	public int getYear() {
 		return Integer.parseInt(date.substring(6, 10));
 	}
@@ -39,30 +33,7 @@ public class Evaluation {
 		this.date = date;
 	}
 
-	public int getSkillA() {
-		return skillA;
-	}
-
-	public void setSkillA(int skillA) {
-		this.skillA = skillA;
-	}
-
-	public int getSkillB() {
-		return skillB;
-	}
-
-	public void setSkillB(int skillB) {
-		this.skillB = skillB;
-	}
-
-	public int getSkillC() {
-		return skillC;
-	}
-
-	public void setSkillC(int skillC) {
-		this.skillC = skillC;
-	}
-
+	
 	public static List<Evaluation> getEmpEvals() {
 		return empEvals;
 	}
@@ -70,11 +41,16 @@ public class Evaluation {
 	public static void setEmpEvals(List<Evaluation> empEvals) {
 		Evaluation.empEvals = empEvals;
 	}
+	
+	public Integer getScore(String skill) {
+		return skills.get("skill");
+	}
 
 	@Override
 	public String toString() {
-		return String.format("ID: %d\r\nDate: %s\r\nA: %d\r\nB: %d\r\nC: %d\r\n", empID, date, skillA, skillB, skillC);
+		return "Evaluation [empID=" + empID + ", date=" + date + ", skills=" + skills + "]";
 	}
+
 
 	public static List<Evaluation> empEvals = new ArrayList<Evaluation>();
 
